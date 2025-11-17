48955c8d63eb91bb3ba5785de86e948da0c745f9
Yahya Lahkim <lahkimyahya1@gmail.com>
2025-11-17 17:10:47 +0100

    Création de la 1er vue recette

---
+++ RecettesApp/RecetteView.swift
@@ -0,0 +1,18 @@
+//
+//  RecetteView.swift
+//  RecettesApp
+//
+//  Created by Yahya lahkim on 17/11/2025.
+//
+
+import SwiftUI
+
+struct RecetteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecetteView()
+}
