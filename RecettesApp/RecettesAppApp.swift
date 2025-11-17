c2694b6b0d1a24673f661bf7f42cb866c2833bc1
Yahya Lahkim <lahkimyahya1@gmail.com>
2025-11-17 16:44:58 +0100

    Initial Commit

---
+++ RecettesApp/RecettesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecettesAppApp.swift
+//  RecettesApp
+//
+//  Created by Yahya lahkim on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecettesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
